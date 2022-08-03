eeb06ad8d3c64e558088c4bf1bc5cdce7f804a0d
Elias McCoy <emccoy1700@gmail.com>
2022-08-03 09:42:17 -0400

    Added solution to leetcode 729 MyCalendar1
    This solution uses the efficient approach of binary search via a recursive function call of insert()

---
+++ 729.MyCalendar1/729.MyCalendar1.py
@@ -0,0 +1,19 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.books = []
+
+    def book(self, start: int, end: int)  -> bool:
+        for s, e in self.books:
+            if s < end and start < e:
+                return False
+
+        self.books.append((start, end))
+        return True
+
+ # Your MyCalendar object will be instantiated and called as such:
+ # obj = MyCalendar()
+ # param_1 = obj.book(start,end)
+
+ #Instead of the naive linear approach we can use a binary search to optimize
+ #our runtime
---
+++ 729.MyCalendar1/729.MyCalendar1efficient.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = None
+        self.right = None
+
+    def insert(self, node):
+        if node.end <= self.start:
+            if not self.left:
+                self.left = node
+                return True
+            else:
+                return self.left.insert(node)
+        elif self.end <= node.start:
+            if not self.right:
+                self. right = node
+                return True
+            else:
+                return self.right.insert(node)
+        else:
+            return False
+class MyCalendar:
+    def __init__(self):
+        self.root = None
+
+    def book(self, start: int, end: int) -> bool:
+        if not self.root:
+            self.root = Node(start, end)
+            return True
+        else:
+            return self.root.insert(Node(start,end))
+
+ # Your MyCalendar object will be instantiated and called as such:
+ # obj = MyCalendar()
+ # param_1 = obj.book(start,end)
